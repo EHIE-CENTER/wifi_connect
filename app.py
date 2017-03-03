88b8a3f07bae0dad8840a12cc91087a927756a10
Philip Lundrigan <philiplundrigan@gmail.com>
2017-03-03 06:29:25 +0000

    Finish wifi setup

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-import subprocess
+# import subprocess
+# from threading import Thread
+from multiprocessing import Process, Queue
+from queue import Empty
 import time
 
-from flask import Flask, render_template, send_from_directory, request
+from eventlet import greenthread
+from eventlet.green import subprocess
+import eventlet
+eventlet.monkey_patch()
+from flask import Flask, jsonify, render_template, send_from_directory, request, copy_current_request_context
 from flask_socketio import SocketIO, emit
 from wifi import Cell, Scheme
 
@@ -9,7 +16,8 @@ INTERFACE = 'ra0'
 SCHEME = 'prisms'
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, logger=True, engineio_logger=True)
+queue = Queue()
 
 
 @app.route('/js/<path:path>')
@@ -25,16 +33,11 @@ def main():
 @socketio.on('wifi-status')
 def handle_wifi_status():
     try:
-        output = subprocess.check_output(['iwconfig', INTERFACE], stderr=subprocess.STDOUT)
-    except CalledProcessError as e:
-        print(e)
-        emit('wifi-status', {'message': 'Error checking status'})
-        return
-
-    output = output.decode().split('\n')
-    output = [o for o in output if 'Access Point' in o]
-    connected = all(['Not-Associated' not in o for o in output])
-    emit('wifi-status', {'message': 'Connected' if connected else 'Not Connected'})
+        output = subprocess.check_output(['ip', 'addr', 'show', INTERFACE])
+        output = output.decode().split("inet ")[1].split("/")[0]
+        emit('wifi-status', {'message': 'Connected ({})'.format(output)})
+    except IndexError as e:
+        emit('wifi-status', {'message': 'Not Connected'})
 
 
 @socketio.on('wifi-get')
@@ -58,16 +61,62 @@ def handle_wifi_scan():
 
 @socketio.on('wifi-update')
 def handle_wifi_update(data):
-    if 'ssid' not in data or 'password' not in data:
-        emit('wifi-update', {'message': 'Invalid request to wifi-update'})
+    if 'ssid' not in data or len(data['ssid']) == 0:
+        emit('wifi-update', {'message': 'Network name must be provided'})
+        return
 
-    emit('wifi-update', {'message': 'Updating...'})
-    # TODO: update WiFi
+    if 'password' not in data or len(data['password']) == 0:
+        emit('wifi-update', {'message': 'Password must be provided'})
+        return
 
-    # Update the status of WiFi
-    handle_wifi_status()
+    ssid = data['ssid']
+    password = data['password']
+
+    # Find a network with this name
+    emit('wifi-update', {'message': 'Looking for network...'})
+    eventlet.sleep(.2)
+    cells = Cell.where(INTERFACE, lambda x: x.ssid == ssid)
+
+    if len(cells) == 0:
+        emit('wifi-update', {'message': 'No network named {}'.format(ssid)})
+        return
+
+    emit('wifi-update', {'message': 'Saving network name and password...'})
+    eventlet.sleep(.2)
+    scheme = Scheme.for_cell(INTERFACE, SCHEME, cells[0], password)
+    scheme.delete()
+    scheme.save()
+
+    emit('wifi-status', {'message': 'Not Connected'})
+    emit('wifi-update', {'message': 'Connecting...'})
+    eventlet.sleep(.2)
+
+    def activate():
+        try:
+            result = scheme.activate()
+            queue.put(('wifi-update', {'message': 'Connected!'}))
+            queue.put(('wifi-status',
+                       {'message': 'Connected ({})'.format(result.ip_address)}))
+        except Exception as e:
+            print(e)
+            queue.put(('wifi-update',
+                       {'message': 'Unable to connect to network. Make sure '
+                                   'password is correct.'}))
+
+    p = Process(target=activate)
+    p.start()
+
+
+def check_processes():
+    while True:
+        try:
+            name, data = queue.get_nowait()
+            print("Emitting {} {}".format(name, data))
+            socketio.emit(name, data)
+        except Empty:
+            eventlet.sleep(1)
 
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=3210, debug=True)
-
+    eventlet.spawn_n(check_processes)
+    socketio.run(app, host='0.0.0.0', port=3210)
