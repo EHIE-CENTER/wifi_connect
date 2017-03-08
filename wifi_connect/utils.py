9b34f028018ded5f8510f0554692d526ac5ac6a0
Philip Lundrigan <philiplundrigan@gmail.com>
2017-03-07 19:38:16 -0700

    Add better logging to utils

--- wifi_connect/utils.py
+++ wifi_connect/utils.py
@@ -6,9 +6,13 @@ _LOGGER = logging.getLogger(__name__)
 
 async def restart_sensor_service():
     try:
-        cmd = asyncio.create_subprocess_exec('systemctl', 'restart', 'sensor.service')
+        cmd = asyncio.create_subprocess_exec('systemctl', 'restart', 'sensor.service',
+                                             stdout=asyncio.subprocess.PIPE,
+                                             stderr=asyncio.subprocess.PIPE)
         proc = await cmd
-        await proc.wait()
+        stdout_data, stderr_data = await proc.communicate()
+        _LOGGER.debug("stdout: %s", stdout_data)
+        _LOGGER.debug("stderr: %s", stderr_data)
         return True
     except Exception as e:
         _LOGGER.exception("Failed to restart service: %s", e)
